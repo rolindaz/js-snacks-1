c611218ce08a4117124b5526ca21eaa0cfd1ffb7
rolindaz <rolinda.zamboni@gmail.com>
2025-05-30 11:32:41 +0200

    Add HTML and JavaScript for word comparison functionality

--- js-snack-1/script.js
+++ js-snack-1/script.js
@@ -1,8 +1,11 @@
-// L'utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+// L'utente inserisce due numeri in successione, con due prompt. 
 
 // L'utente inserisce due numeri in successione, con due prompt.
 
 let userNumber1 = Number(prompt('Pick your first number')), userNumber2 = Number(prompt('Pick your second number'));
+
+// Il software stampa il maggiore.
+
 if (userNumber1 > userNumber2) {
     console.log(userNumber1);
 } else if (userNumber2 > userNumber1) {
